4c59e6ae6f816d3d06e0eee770f761adf5fae28b
mimi544 <59132699+mimi544@users.noreply.github.com>
2022-09-13 23:58:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import { Header,CreateContainer,MainContainer} from "./components";
 
 const App = () => {
   return (
-    <div className="w-screen h-auto flex flex-col bg-cyan-300">
-      <Header />
-      <main className="mt-24 p-8 w-full"> 
-        <Routes>
-          <Route path="/*" element={<MainContainer/>}/>
-          <Route path="/createItem" element={<CreateContainer/>}/>
-        </Routes>
-      </main>
-    </div>
+    <Router>
+      <div className="w-screen h-auto flex flex-col bg-cyan-300">
+        <Header />
+        <main className="mt-24 p-8 w-full">
+          <Routes>
+            <Route path="/*" element={<MainContainer />} />
+            <Route path="/createItem" element={<CreateContainer />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 };
 
