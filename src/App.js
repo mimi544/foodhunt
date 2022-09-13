84dbbf22507bcf7d03a8be02704236520ae46e9d
AdityaChowdhury2 <adityac5102@gmail.com>
2022-09-13 23:32:56 +0530

    changes in master

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+<<<<<<< HEAD
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Header, CreateContainer, MainContainer } from "./components";
+=======
+import { Route, Routes } from "react-router-dom";
+import { Header,CreateContainer,MainContainer} from "./components";
+>>>>>>> main
 //import { Header } from './components';//
 
 const App = () => {
   return (
+<<<<<<< HEAD
     <Router>
       <div className="w-screen h-auto flex flex-col bg-cyan-300">
         <Header />
@@ -16,6 +22,17 @@ const App = () => {
         </main>
       </div>
     </Router>
+=======
+    <div className="w-screen h-auto flex flex-col bg-cyan-300">
+      <Header />
+      <main className="mt-24 p-8 w-full"> 
+        <Routes>
+          <Route path="/*" element={<MainContainer/>}/>
+          <Route path="/createItem" element={<CreateContainer/>}/>
+        </Routes>
+      </main>
+    </div>
+>>>>>>> main
   );
 };
 
--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const MainContainer = () => {
   return (
     <div>
+<<<<<<< HEAD
       <p>Hello World</p>
+=======
+      
+>>>>>>> main
     </div>
   )
 }
