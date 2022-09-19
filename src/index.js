825530c09e0ac2f04940894cb00e1c3e7bca2e37
mimi544 <59132699+mimi544@users.noreply.github.com>
2022-09-19 12:38:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import './index.css'
 import App from "./App";
-import { StateProvider } from "./context/StateProvider";
-import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( <StateProvider initialState={initialState} reducer={reducer} />);
+root.render(<App />);
